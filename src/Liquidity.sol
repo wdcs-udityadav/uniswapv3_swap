206c602df4554a44a300411ae3cdcd8bb0bed9fb
Udit Yadav <udit.yadav@codezeros.com>
2024-02-01 19:04:05 +0530

    fixes

--- src/Liquidity.sol
+++ src/Liquidity.sol
@@ -11,36 +11,36 @@ import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 import "@uniswap/v3-periphery/contracts/base/LiquidityManagement.sol";
 
 contract Liquidity is IERC721Receiver, LiquidityManagement {
-    address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    // address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    // address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
-    uint24 public constant poolFee = 3000;
+    // uint24 public constant poolFee = 3000;
 
-    INonfungiblePositionManager public constant nonfungiblePositionManager =
-        INonfungiblePositionManager(0xC36442b4a4522E871399CD717aBDD847Ab11FE88);
+    // INonfungiblePositionManager public constant nonfungiblePositionManager =
+    //     INonfungiblePositionManager(0xC36442b4a4522E871399CD717aBDD847Ab11FE88);
 
-    struct Deposit {
-        address owner;
-        uint256 liquidity;
-        address token0;
-        address token1;
-    }
+    // struct Deposit {
+    //     address owner;
+    //     uint256 liquidity;
+    //     address token0;
+    //     address token1;
+    // }
 
-    mapping(uint256 => Deposit) public deposits;
+    // mapping(uint256 => Deposit) public deposits;
 
-    constructor() {}
+    // constructor() {}
 
-    function onERC721Received(address operator, address, uint256 tokenId, bytes calldata data)
-        external
-        override
-        returns (bytes4)
-    {
-        _createDeposit(operator, tokenId);
-        return this.onERC721Received.selector;
-    }
+    // function onERC721Received(address operator, address, uint256 tokenId, bytes calldata data)
+    //     external
+    //     override
+    //     returns (bytes4)
+    // {
+    //     _createDeposit(operator, tokenId);
+    //     return this.onERC721Received.selector;
+    // }
 
-    function _createDeposit(address owner, uint256 _tokenId) private {
-        (,, address token0, address token1,,,, uint128 liquidity,,,,) = nonfungiblePositionManager.positions(_tokenId);
-        deposits[_tokenId] = Deposit({owner: owner, liquidity: liquidity, token0: token0, token1: token1});
-    }
+    // function _createDeposit(address owner, uint256 _tokenId) private {
+    //     (,, address token0, address token1,,,, uint128 liquidity,,,,) = nonfungiblePositionManager.positions(_tokenId);
+    //     deposits[_tokenId] = Deposit({owner: owner, liquidity: liquidity, token0: token0, token1: token1});
+    // }
 }
